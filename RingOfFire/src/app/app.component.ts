660803d02d28d3ceaf891c9363847e5ab5d37e64
jjb6000 <jonas.benkwitz@gmail.com>
2025-06-06 12:21:32 +0200

    scss startcompo + routing

--- RingOfFire/src/app/app.component.ts
+++ RingOfFire/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
