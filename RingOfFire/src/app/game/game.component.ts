753ac55d3cfa199725a0e5da977d8b78ffa22c29
jjb6000 <jonas.benkwitz@gmail.com>
2025-06-30 18:13:07 +0200

    mat Dialog added

--- RingOfFire/src/app/game/game.component.ts
+++ RingOfFire/src/app/game/game.component.ts
@@ -1,19 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, ChangeDetectionStrategy, inject, model, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import {
-  MAT_DIALOG_DATA,
-  MatDialog,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogRef,
-  MatDialogTitle,
-} from '@angular/material/dialog';
+import { PlayerDialog } from '../player-dialog/player-dialog.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 export interface DialogData {
   name: string;
@@ -21,7 +13,7 @@ export interface DialogData {
 
 @Component({
   selector: 'app-game',
-  imports: [CommonModule, PlayerComponent, MatIconModule, MatButtonModule],
+  imports: [CommonModule, PlayerComponent, MatIconModule, MatButtonModule, MatDialogModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -29,23 +21,15 @@ export class GameComponent implements OnInit {
   pickedCardAnimation = false;
   game: Game | undefined
   currentCard: string | undefined;
-  readonly dialog = inject(MatDialog);
-  readonly dialogRef = inject(MatDialogRef<AddPlayerDialog>);
-  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
-  readonly name = model(this.data.name);
 
-  constructor() { }
+
+  constructor(public dialog: MatDialog) { }
 
   openDialog(): void {
-    const dialogRef = this.dialog.open(AddPlayerDialog, {
-      data: { name: this.name() },
-    });
+    const dialogRef = this.dialog.open(PlayerDialog);
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe(() => {
       console.log('The dialog was closed');
-      if (result !== undefined) {
-        this.name.set(result);
-      }
     });
   }
 
@@ -65,15 +49,7 @@ export class GameComponent implements OnInit {
 }
 
 
-export class AddPlayerDialog {
-  readonly dialogRef = inject(MatDialogRef<AddPlayerDialog>);
-  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
-  readonly name = model(this.data.name);
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
-}
 
 
 
---
+++ RingOfFire/src/app/player-dialog/player-dialog.component.ts
@@ -0,0 +1,24 @@
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+export interface DialogData {
+  name: string;
+}
+
+@Component({
+  selector: 'player-dialog',
+  templateUrl: 'player-dialog.component.html',
+  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PlayerDialog {
+  readonly dialogRef = inject(MatDialogRef<PlayerDialog>);
+}
