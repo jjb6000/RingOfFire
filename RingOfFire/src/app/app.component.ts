0c05dc32f39a57a3b528f4ec4593c9e1b565e0ce
jjb6000 <jonas.benkwitz@gmail.com>
2025-07-27 11:03:03 +0200

    setup fire wip

--- RingOfFire/src/app/app.component.ts
+++ RingOfFire/src/app/app.component.ts
@@ -5,13 +5,17 @@ import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
+import { AsyncPipe } from '@angular/common';
+import { Firestore, collection, collectionData } from '@angular/fire/';
+
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatDividerModule, RouterOutlet],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatDividerModule, RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  firestore: Firestore = inject(Firestore);
   title = 'RingOfFire';
 }
